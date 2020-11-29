67da3db2282c8cfc7cc1468507f89c4a77b5a7e8
Azel04 <75199595+Azel04@users.noreply.github.com>
2020-11-29 18:18:56 +0100

    Ora non ripete i numeri sull'output finale

--- VettoriComuni.c
+++ VettoriComuni.c
@@ -8,13 +8,17 @@ int main()
     int v1[N], v2[M], v3[M], i1, i2, i3, i3fin;
     for(i1 = 0 ; i1 < N; i1++)
     {
-        printf("Inserisci l'elemento %d del primo vettore.\n",i1 + 1);
+        do{
+        printf("Inserisci l'elemento %d del primo vettore. NON INSERIRE IL VALORE -999!\n",i1 + 1);
         scanf("%d",&v1[i1]);
+        }while(v1[i1] == -999);
     }
     for(i2 = 0 ;i2 < M; i2++)
     {
-        printf("Inserisci l'elememto %d del secondo vettore.\n",i2 + 1);
+        do{
+        printf("Inserisci l'elememto %d del secondo vettore. NON INSERIRE IL VALORE -999!\n",i2 + 1);
         scanf("%d",&v2[i2]);
+        }while(v2[i2] == -999);
     }
 
     i1 = 0;
@@ -31,8 +35,19 @@ int main()
         }
     }
     i3fin = i3;
+
+    for(i2 = 0; i2 < i3fin; i2++){
+        for(i1 = 0; i1 < i3fin; i1++){
+            if(v3[i2] == v3[i1] && i1 != i2){
+                v3[i1] = -999;
+            }
+        }
+    }
+
     for(i1 = 0; i1 < i3fin; i1++){
-    printf("%d\n",v3[i1]);
+        if(!(v3[i1] == -999)){
+            printf("%d\n",v3[i1]);
+        }
     }
 
     return 0;
